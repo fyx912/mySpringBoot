e1c37feafb4ada0a2a781bff45fa255b30fa4692
ding <fyx912@163.com>
2018-04-06 17:19:30 +0800

    spring boot

---
+++ springBoot/src/main/java/com/ding/AppMain.java
@@ -0,0 +1,22 @@
+package com.ding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class AppMain {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(AppMain.class,args);
+    }
+}
