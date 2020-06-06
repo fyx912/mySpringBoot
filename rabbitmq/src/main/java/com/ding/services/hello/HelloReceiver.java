81e9f1c58c798b0809778f9a4c27b8b3262e154d
Tintin <fyx912@163.com>
2020-06-06 15:19:27 +0800

    update

--- rabbitmq/src/main/java/com/ding/services/hello/HelloReceiver.java
+++ rabbitmq/src/main/java/com/ding/services/hello/HelloReceiver.java
@@ -2,7 +2,6 @@ package com.ding.services.hello;
 
 
 import com.rabbitmq.client.Channel;
-import com.sun.org.apache.xpath.internal.operations.String;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
